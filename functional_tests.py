6c108c64fab09a918dd62a1bafd9597ce36e1f56
yaoelvon <yaoelvon@gmail.com>
2017-02-10 23:37:42 +0800

    [fy] 前端的html页面，从template生成

--- functional_tests.py
+++ functional_tests.py
@@ -38,7 +38,8 @@ class NewVisitorTest(unittest.TestCase):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows)
+			any(row.text == '1: Buy peacock feathers' for row in rows),
+			"New to-do item did not appear in table"
 			)
 
 		# 页面上又显示了一个文本框，可以输入其他待办事项
